5f53a0ab65ff319fb877a43eaecad5f902528156
asiloro <66637621+asiloro@users.noreply.github.com>
2020-06-19 16:53:39 +0100

    Update test_calculadora.py

--- test/carteiropy/test_calculadora.py
+++ test/carteiropy/test_calculadora.py
@@ -9,4 +9,4 @@ class TestSum(unittest.TestCase):
 
         
     def test_soma(self):
-        self.assertEqual(calculadora.soma(3,9), 8, "Should be 8")
+        self.assertEqual(calculadora.soma(3,8), 11, "Should be 11")
