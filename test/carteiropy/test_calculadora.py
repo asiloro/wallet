b54a6b8f26bb04d37cb2b6293b995ae0715d9b6e
APrata <66637621+asiloro@users.noreply.github.com>
2020-09-19 19:16:56 +0100

    Update test_calculadora.py

--- test/carteiropy/test_calculadora.py
+++ test/carteiropy/test_calculadora.py
@@ -5,4 +5,4 @@ from carteiropy import calculadora
 class TestSum(unittest.TestCase):
 
     def test_soma(self):
-        self.assertEqual(calculadora.soma(3,8), 11, "O teste falhou, a soma dos dois numeros não resula em 11")
+        self.assertEqual(calculadora.soma(2,8), 11, "O teste falhou, a soma dos dois numeros não resula em 11")
