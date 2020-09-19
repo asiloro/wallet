c182415531e50bd9169e365641731430a5ae0aa3
APrata <66637621+asiloro@users.noreply.github.com>
2020-09-19 17:02:07 +0100

    Update test_calculadora.py

--- test/carteiropy/test_calculadora.py
+++ test/carteiropy/test_calculadora.py
@@ -6,7 +6,4 @@ class TestSum(unittest.TestCase):
 
     def test_soma(self):
         self.assertEqual(calculadora.soma(2,6), 8, "Should be 8")
-
-        
-    def test_soma(self):
         self.assertEqual(calculadora.soma(3,8), 11, "Should be 11")
